44c75d458da42f613ce3569972a0647f43399c3f
Dante Bautista <dante@landgorilla.com>
2021-02-21 12:19:10 -0500

    2

--- webhook.php
+++ webhook.php
@@ -1,7 +1,8 @@
 <?php
+  require __DIR__ .  '/vendor/autoload.php';
 
-MercadoPago\SDK::setAccessToken("APP_USR-8208253118659647-112521-dd670f3fd6aa9147df51117701a2082e-677408439");
+  // MercadoPago\SDK::setAccessToken("APP_USR-8208253118659647-112521-dd670f3fd6aa9147df51117701a2082e-677408439");
 
-file_put_contents('log.txt', file_get_contents('php://input'), FILE_APPEND);
+  file_put_contents('log.txt', file_get_contents('php://input'), FILE_APPEND);
 
 ?>
