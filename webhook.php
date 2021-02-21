21a1be0ff6a7472cf57c115c1c3cabb931eeca68
Dante Bautista <dante@landgorilla.com>
2021-02-21 12:23:17 -0500

    2

--- webhook.php
+++ webhook.php
@@ -3,6 +3,6 @@
 
   // MercadoPago\SDK::setAccessToken("APP_USR-8208253118659647-112521-dd670f3fd6aa9147df51117701a2082e-677408439");
 
-  file_put_contents('log.txt', file_get_contents('php://input'), FILE_APPEND);
-
+  // file_put_contents('log.txt', file_get_contents('php://input'), FILE_APPEND);
+  mail("sdabrf@gmail.com", "webhook", file_get_contents('php://input'));
 ?>
